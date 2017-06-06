de63f1328eca4bb1081c0796b142ec51a8e36180
Cait Rocco <cait.rocco@gmail.com>
2017-06-06 14:01:04 -0500

    completed exercise

--- reindeer.js
+++ reindeer.js
@@ -1,5 +1,10 @@
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
+var loopCount = 8;
 
 var hohohoElement = document.getElementById("reindeer");
+
+for (var i = 0; i < loopCount; i++) {
+	hohohoElement.innerHTML += colors[i] + " " + reindeer[i] + "<br>";
+}
